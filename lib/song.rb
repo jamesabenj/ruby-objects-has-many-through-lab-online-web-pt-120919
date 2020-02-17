3d26bf6bfb5528029152f351115ba484565d2502
James Benjamin <james.abenj@gmail.com>
2020-02-17 00:56:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,10 @@ class Song
   attr_accessor :name, :genre, :artist
 
   @@all = []
-  def initialize(name, genre)
+  def initialize(name, artist, genre)
     @name = name
     @genre = genre
+    @artist = artist 
     @@all << self
   end
 
